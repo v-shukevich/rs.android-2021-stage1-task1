b49794afe663f3929fc14101f26a3497c186d9af
Vitali Shukevich <vshukevich@gmail.com>
2022-02-02 01:00:34 +0300

    algoritm1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,17 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
+        var happyArray : IntArray = IntArray(0)
+        happyArray += sadArray[0]
+        for (i in 1..sadArray.size-2){
+            if (sadArray[i-1] + sadArray[i+1] > sadArray[i]){
+                happyArray += sadArray[i]
+
+            }
+        }
+        happyArray += sadArray[sadArray.size-1]
+        return happyArray
         throw NotImplementedError("Not implemented")
+
     }
 }
